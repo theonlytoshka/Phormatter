0ac9d5320ddfd70b8f07c986facbdae801cbc054
Anton Pashko <anton@pashko.org>
2021-06-21 13:02:19 -0400

    Added counter

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,10 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Controller {
 
@@ -9,6 +13,19 @@ public class Controller {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label_counter;
+
+    int count = 0;
+    @FXML
+    private void onClick(ActionEvent event) {
+        count++;
+        label_counter.setText(String.valueOf(count));
+    }
 
     @FXML
     void initialize() {
